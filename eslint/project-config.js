14b398900af6707d7bcd415e01fd617376eb68d1
rone <robertneagu194@gmail.com>
2025-02-12 18:12:11 +0000

    Add comments for rules

--- eslint/project-config.js
+++ eslint/project-config.js
@@ -1,10 +1,11 @@
 export default {
   rules: {
+    // @override: Enforce strictness for all types of lines
     // Specify curly brace conventions for all control statements
     // https://eslint.org/docs/rules/curly
     curly: ['error'],
 
-    // Require switch-case statements to be exhaustive.
+    // Require switch-case statements to be exhaustive
     // https://typescript-eslint.io/rules/switch-exhaustiveness-check/
     '@typescript-eslint/switch-exhaustiveness-check': 'error',
 
@@ -12,9 +13,9 @@ export default {
     // https://github.com/import-js/eslint-plugin-import/blob/main/docs/rules/order.md
     'import/order': ['error', { groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'] }],
 
+    // @override: Turned off because of the way vite handles imports from public directory
     // Forbid import of modules using absolute paths
     // https://github.com/import-js/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
-    // Turned off because of the way vite handles imports from public directory
     'import/no-absolute-path': 'off',
   },
 };
